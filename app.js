e4f68eca3a88382a7dbb61f576de4966bce3fbc2
Darwin Rodriguez <darwinshopper@gmail.com>
2025-03-12 10:39:56 -0500

    Se Agregó la función para capturar datos, validar datos de entrada y que no sean repetidos, actualizar el array de amigos y limpiar el campo de entrada

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let input = document.getElementById("amigo");
+    let nombre = input.value.trim();
+
+    if (nombre === "") {
+        alert ("Por favor ingrese un nombre");
+        return;
+    }
+
+    let regex = /^[A-Za-z\s]+$/;
+    if (!regex.test(nombre)) {
+        alert("Solo se permiten nombres con letras, sin números.");
+        return;
+    }
+
+    if (amigos.includes(nombre)) {
+        alert("Este nombre ya ha sido agregado.");
+        return;
+    }
+
+    amigos.push(nombre);
+
+    actulizarLista();
+
+    input.value = "";
+}
+
+function actulizarLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+
+    amigos.forEach((nombre) => {
+        let li = document.createElement("li");
+        li.textContent = nombre;
+        lista.appendChild(li);
+    })
+}
