fb9e7213242e00b7ef49aa914b964f0ffaaff806
Darwin Rodriguez <darwinshopper@gmail.com>
2025-03-14 00:01:43 -0500

    Se creo la función sortearAmigos, validación que la lista sea par, que haya elementos en la lista, que al realizar el sorteo no se repitan los nombres que ya han sido sorteados, que aparesca en pantalla que el nombre sorteo por 2 segundos

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
+let nombresSorteados = [];
 
 function agregarAmigo() {
     let input = document.getElementById("amigo");
@@ -26,7 +27,14 @@ function agregarAmigo() {
     actulizarLista();
 
     input.value = "";
+    input.focus();
 }
+
+document.getElementById("amigo").addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        agregarAmigo();
+    }
+})
 
 function actulizarLista() {
     let lista = document.getElementById("listaAmigos");
@@ -38,3 +46,41 @@ function actulizarLista() {
         lista.appendChild(li);
     })
 }
+
+function sortearAmigo() {
+    if (amigos.length % 2 !== 0) {
+        alert("Debe haber un número par de amigos para hacer el sorteo");
+        return;
+    }
+    
+    if (amigos.length === 0) {
+        alert("No hay nombres que sotear");
+        return;
+    }
+   
+    amigos.sort(() => Math.random() - 0.5);
+
+   if (nombresSorteados.length === amigos.length) {
+        alert("Todos los nombres han sido sorteados. Se reiniciará el sorteo.");
+        nombresSorteados = []; 
+   }
+
+   let amigoSorteado;
+   do {
+        let amigoAleatorio = Math.floor(Math.random() * amigos.length);
+        amigoSorteado = amigos[amigoAleatorio];
+   } while (nombresSorteados.includes(amigoSorteado));
+
+   nombresSorteados.push(amigoSorteado);
+
+   let resultado = document.getElementById("resultado");
+
+   resultado.innerHTML = `Sorteado: ${amigoSorteado}`;
+   
+   setTimeout(() => {
+        resultado.innerHTML = "";
+   }, 2000);
+    
+}
+
+document.querySelector(".button-draw").addEventListener("click", sortearAmigos);
